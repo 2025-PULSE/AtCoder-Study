f6e0b5bfdbdc4cc69cb4dd189d1225e7a3b2de06
kang <kanghy6220@gmail.com>
2025-10-06 17:15:17 +0900

    C: AC(대회 후)

--- ABC426/RainDrop3/C.cpp
+++ ABC426/RainDrop3/C.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 
-int main() {
-    int N, Q, x, y, cnt;
-    cin >> N >> Q;
-    vector<int> v;
-    v.reserve(N);
+int find_set(int v, vector<int>& parent) {
+    if (v == parent[v]) return v;
+    return parent[v] = find_set(parent[v], parent);
+}
 
-    for (int i=1; i<=N; ++i) v.push_back(i);
+void union_set(int a, int b, vector<int>& parent) {
+    a = find_set(a, parent);
+    b = find_set(b, parent);
+    if (a != b) parent[a] = b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, Q, x, y;
+    cin >> N >> Q;
+
+    vector<int> parent(N+2);
+    iota(parent.begin(), parent.end(), 0);
+
+    vector<long long> count(N+2, 1);
 
     while (Q--) {
-        cnt = 0;
         cin >> x >> y;
-        for (int i=0; i<x; ++i) {
-            if (v[i] <= x) {
-                v[i] = y;
-                cnt++;
-            }
+        long long cnt = 0;
+
+        for (int v = find_set(1, parent); v <= x; v=find_set(v, parent)) {
+            cnt += count[v];
+            count[y] += count[v];
+            count[v] = 0;
+
+            union_set(v, v+1, parent);
         }
         cout << cnt << '\n';
     }
