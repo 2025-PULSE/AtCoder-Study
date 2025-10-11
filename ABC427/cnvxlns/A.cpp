7d699d70873cd1442f977b1847950b57a2173213
hyeonK <157864988+okmac03@users.noreply.github.com>
2025-10-11 22:45:58 +0900

    A : AC

---
+++ ABC427/cnvxlns/A.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+#define Size 200032
+#define inf 0x3f3f3f3f
+#define endl "\n"
+#define sp " "
+#define int int64_t
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i,a,b) for(int i=(int)(a);i<=(int)(b);i++)
+
+const int MOD = 1e9 + 7;
+
+using namespace std;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+using pic = pair<int, char>;
+using pci = pair<char, int>;
+using vpii = vector<pii>;
+using vpic = vector<pic>;
+using vpci = vector<pci>;
+using vtiii = vector<tiii>;
+
+pii operator+(const pii& a, const pii& b) {
+    return { a.first + b.first, a.second + b.second };
+}
+pii operator-(const pii& a, const pii& b) {
+    return { a.first - b.first, a.second - b.second };
+}
+
+pii dxy[] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+void Solve() {
+    string s;
+    cin >> s;
+    for(auto i = 0; i < s.size(); ++i){
+        if(i == (s.size() + 1) / 2 -1){
+            continue;
+        }
+        cout << s[i];
+    }
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    Solve();
+    return 0;
+}
